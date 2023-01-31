9cf2cf94ec27e03292a09f837128f21104e9adbc
fyirass <firas.mansour.pro@gmail.com>
2023-01-31 22:42:48 +0100

    +pipe

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -53,15 +53,15 @@ const [center, setCoordinates] = useState([-71.3583, 50.1686]);
       });
   }, []);
 
-
+  const accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
   useEffect(() => {
     if (sensorsData.length > 0) {
-      mapboxgl.accessToken = 'pk.eyJ1IjoiZmFobWloOTYiLCJhIjoiY2t1cXRkNWt2MGtxNjJucXZlN2FxemNpZiJ9.zBOiFS6ym4zFF9ZQ7zcmXA';
+      mapboxgl.accessToken = accessToken;
       const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v12',
         center: center[0].map_coordinate,
-        zoom: 10
+        zoom: 12
       });
 
       map.on('load', () => {
@@ -79,64 +79,46 @@ const [center, setCoordinates] = useState([-71.3583, 50.1686]);
             .addTo(map);
           });
           
-       
-        map.addSource('route', {
-          'type': 'geojson',
-          'data': {
-          'type': 'Feature',
-          'properties': {},
-          'geometry': {
-          'type': 'LineString',
-          'coordinates': [
-          [-122.483696, 37.833818],
-          [-122.483482, 37.833174],
-          [-122.483396, 37.8327],
-          [-122.483568, 37.832056],
-          [-122.48404, 37.831141],
-          [-122.48404, 37.830497],
-          [-122.483482, 37.82992],
-          [-122.483568, 37.829548],
-          [-122.48507, 37.829446],
-          [-122.4861, 37.828802],
-          [-122.486958, 37.82931],
-          [-122.487001, 37.830802],
-          [-122.487516, 37.831683],
-          [-122.488031, 37.832158],
-          [-122.488889, 37.832971],
-          [-122.489876, 37.832632],
-          [-122.490434, 37.832937],
-          [-122.49125, 37.832429],
-          [-122.491636, 37.832564],
-          [-122.492237, 37.833378],
-          [-122.493782, 37.833683]
-          ]
-          }
-          }
-          });
-      
-          map.addLayer({
-          'id': 'route',
-          'type': 'line',
-          'source': 'route',
-          'layout': {
-          'line-join': 'round',
-          'line-cap': 'round'
-          },
-          'paint': {
-          'line-color': '#888',
-          'line-width': 8
-          }
-          });
-        });
-      setMap(map);
-    }
-  }, [sensorsData]);
 
-  return (
-    <div>
-      <div id="map" style={{ width: '99vw', height: '88vh' }} />
-    </div>
-  );
+  pipesData.forEach((pipe) => {
+    const coordinates = pipe.pipe_coordinates;
+    // create a GeoJSON feature with the pipe coordinates
+    const pipeFeature = {
+      type: 'Feature',
+      geometry: {
+        type: 'LineString',
+        coordinates: coordinates,
+      },
+      properties: {},
+    };
+    // add the pipe feature to the map
+    map.addSource('pipe-' + pipe.id, {
+      type: 'geojson',
+      data: pipeFeature,
+    });
+    map.addLayer({
+      id: 'pipe-' + pipe.id,
+      type: 'line',
+      source: 'pipe-' + pipe.id,
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round',
+      },
+      paint: {
+        'line-color': '#3284ff',
+        'line-width': 3,
+      },
+    });   
+  });
+});
+}
+}, [sensorsData, pipesData]);
+
+return (
+<div>
+  <div id="map" style={{ width: '99vw', height: '88vh' }} />
+</div>
+);
 };
 
 export default Map;
