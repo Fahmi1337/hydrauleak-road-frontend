1d3f457a9d794de2047c377c853c26b9d28a3a1e
fyirass <firas.mansour.pro@gmail.com>
2023-02-17 19:01:48 +0100

    +test

--- src/components/popups/addpipeaccesspopup/addpipeaccesspopup.js
+++ src/components/popups/addpipeaccesspopup/addpipeaccesspopup.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./addPipeAccessPopup.css"
+import "./addPipeAccessPopup.css";
 import axios from 'axios';
 
 
--- src/containers/Test.js
+++ src/containers/Test.js
@@ -1,137 +1,62 @@
-import React, { useRef, useEffect, useState } from "react";
-import mapboxgl from "mapbox-gl";
-import * as turf from '@turf/turf'
-const Test = () => {
+import React, { useState, useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmFobWloOTYiLCJhIjoiY2t1cXRkNWt2MGtxNjJucXZlN2FxemNpZiJ9.zBOiFS6ym4zFF9ZQ7zcmXA';
+
+const Test = () => {
+  const [map, setMap] = useState(null);
+  const mapContainer = useRef(null);
+  const [marker, setMarker] = useState(null);
+  const [line, setLine] = useState(null);
 
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZmFobWloOTYiLCJhIjoiY2t1cXRkNWt2MGtxNjJucXZlN2FxemNpZiJ9.zBOiFS6ym4zFF9ZQ7zcmXA';
-    const map = new mapboxgl.Map({
-    container: 'map',
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: [2.3399, 48.8555],
-    zoom: 12
+    const newMap = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-122.4194, 37.7749],
+      zoom: 12,
     });
-     
-    const distanceContainer = document.getElementById('distance');
-     
-    // GeoJSON object to hold our measurement features
-    const geojson = {
-    'type': 'FeatureCollection',
-    'features': []
-    };
-     
-    // Used to draw a line between points
-    const linestring = {
-    'type': 'Feature',
-    'geometry': {
-    'type': 'LineString',
-    'coordinates': []
-    }
-    };
-     
-    map.on('load', () => {
-    map.addSource('geojson', {
-    'type': 'geojson',
-    'data': geojson
-    });
-     
-    // Add styles to the map
-    map.addLayer({
-    id: 'measure-points',
-    type: 'circle',
-    source: 'geojson',
-    paint: {
-    'circle-radius': 5,
-    'circle-color': '#000'
-    },
-    filter: ['in', '$type', 'Point']
-    });
-    map.addLayer({
-    id: 'measure-lines',
-    type: 'line',
-    source: 'geojson',
-    layout: {
-    'line-cap': 'round',
-    'line-join': 'round'
-    },
-    paint: {
-    'line-color': '#000',
-    'line-width': 2.5
-    },
-    filter: ['in', '$type', 'LineString']
-    });
-     
-    map.on('click', (e) => {
-    const features = map.queryRenderedFeatures(e.point, {
-    layers: ['measure-points']
-    });
-     
-    // Remove the linestring from the group
-    // so we can redraw it based on the points collection.
-    if (geojson.features.length > 1) geojson.features.pop();
-     
-    // Clear the distance container to populate it with a new value.
-    distanceContainer.innerHTML = '';
-     
-    // If a feature was clicked, remove it from the map.
-    if (features.length) {
-    const id = features[0].properties.id;
-    geojson.features = geojson.features.filter(
-    (point) => point.properties.id !== id
-    );
-    } else {
-    const point = {
-    'type': 'Feature',
-    'geometry': {
-    'type': 'Point',
-    'coordinates': [e.lngLat.lng, e.lngLat.lat]
-    },
-    'properties': {
-    'id': String(new Date().getTime())
-    }
-    };
-     
-    geojson.features.push(point);
-    }
-     
-    if (geojson.features.length > 1) {
-    linestring.geometry.coordinates = geojson.features.map(
-    (point) => point.geometry.coordinates
-    );
-     
-    geojson.features.push(linestring);
-     
-    // Populate the distanceContainer with total distance
-    const value = document.createElement('pre');
-    const distance = turf.length(linestring);
-    value.textContent = `Total distance: ${distance.toLocaleString()}km`;
-    distanceContainer.appendChild(value);
-    console.log("value :", value)
-    }
-     
-    map.getSource('geojson').setData(geojson);
-    });
-    });
-     
-    map.on('mousemove', (e) => {
-    const features = map.queryRenderedFeatures(e.point, {
-    layers: ['measure-points']
-    });
-    // Change the cursor to a pointer when hovering over a point on the map.
-    // Otherwise cursor is a crosshair.
-    map.getCanvas().style.cursor = features.length
-    ? 'pointer'
-    : 'crosshair';
-    });
+
+    setMap(newMap);
+
+    return () => newMap.remove();
   }, []);
+
+  const addMarker = () => {
+    const newMarker = new mapboxgl.Marker()
+      .setLngLat(map.getCenter())
+      .addTo(map);
+
+    setMarker(newMarker);
+  };
+
+  const addLine = () => {
+    const newLine = new mapboxgl.Draw({
+      type: 'line',
+      displayControlsDefault: true,
+      controls: {
+        point: true,
+        trash: true,
+      },
+    });
+
+    map.addControl(newLine);
+
+    newLine.on('create', (e) => {
+      setLine(e.features[0]);
+      map.removeControl(newLine);
+    });
+
+    newLine.changeMode('draw');
+  };
 
   return (
     <div>
-    <div id="map"></div>
-<div id="distance" class="distance-container"></div>
+      <div ref={mapContainer} style={{ width: '100%', height: '500px' }} />
+      <button onClick={addMarker}>Add Marker</button>
+      <button onClick={addLine}>Add Line</button>
     </div>
   );
 };
-export default Test
+
+export default Test;
